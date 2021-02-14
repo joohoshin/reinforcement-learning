a675ca3f6d1ba5042b9f627739d3154ef4f80187
joohoshin <jooho@boozaapp.com>
2021-02-14 20:22:26 +0900

    forked
    dash code, rl library error solved

--- 2. Data Center Cooling/app.py
+++ 2. Data Center Cooling/app.py
@@ -28,7 +28,7 @@ from copy import deepcopy
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output, Event, State
+from dash.dependencies import Input, Output, State
 import plotly.graph_objs as go
 
 import sys
